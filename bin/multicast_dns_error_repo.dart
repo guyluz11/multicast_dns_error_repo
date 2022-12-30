e673184fc03b90cdfa193971c5cfe112003d7925
guyluz11 <guyluz11@gmail.com>
2022-12-30 15:11:35 +0200

    Testing the error without network_tools pacakge

--- bin/multicast_dns_error_repo.dart
+++ bin/multicast_dns_error_repo.dart
@@ -1,5 +1,9 @@
 import 'package:multicast_dns_error_repo/mns_search_devices.dart';
 
 Future<void> main(List<String> arguments) async {
-  await MdnsSearchDevices().searchMdnsDevices();
+  while (true) {
+    await MdnsSearchDevices().testMinimalCode();
+    print('Continue Search');
+    await Future.delayed(Duration(seconds: 60));
+  }
 }
